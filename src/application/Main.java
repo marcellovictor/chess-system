8066ef1f02899bb176f6fefbc76d8080b6143e58
Marcello Victor <marcello.victor.ferreira@hotmail.com>
2020-08-03 15:21:33 -0300

    Projeto criado

---
+++ src/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+		System.out.println("Let's chess!");
+
+	}
+
+}
