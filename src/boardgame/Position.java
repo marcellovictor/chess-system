44b617d7e273c413a1fe048b12bd8860b769c31f
Marcello Victor <marcello.victor.ferreira@hotmail.com>
2020-08-03 17:31:23 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,42 @@
+package boardgame;
+
+public class Position {
+
+	//ATRIBUTOS:
+	
+	private Integer row;
+	private Integer column;
+	
+	//CONSTRUTORES:
+	
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	//GETTERS E SETTERS:
+	
+	public Integer getRow() {
+		return row;
+	}
+
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+	
+	//MÉTODOS:
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+	
+}
