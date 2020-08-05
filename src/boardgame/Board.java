ba581fd70663fa93445e3e38ebe1ac9d87f00aba
Marcello Victor <marcello.victor.ferreira@hotmail.com>
2020-08-05 16:01:54 -0300

    Chess layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -34,6 +34,15 @@ public class Board {
 		this.columns = columns;
 	}
 	
+	//MÉTODOS:
+	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 	
 	
 	
---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,24 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChessPiece extends Piece{
+
+	//ATRIBUTOS:
+	
+	private Color color;
+	
+	//CONSTRUTORES:
+	
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+	
+	//GETTERS E SETTERS:
+	
+	public Color getColor() {
+		return color;
+	}
+}
