35dee397563277a4d05e1a21553d53a8d868b626
Marcello Victor <marcello.victor.ferreira@hotmail.com>
2020-08-26 14:47:15 -0300

    Implementing possible moves of Rook (and fixing bugs)

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -18,6 +19,53 @@ public class Rook extends ChessPiece {
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position auxp = new Position(0, 0);
+		
+		//up:
+		
+		auxp.setValues(position.getRow() - 1, position.getColumn());
+		while (getBoard().positionExists(auxp) && !getBoard().thereIsAPiece(auxp)) {
+			mat[auxp.getRow()][auxp.getColumn()] = true;
+			auxp.setRow(auxp.getRow() - 1);
+		}
+		if (getBoard().positionExists(auxp) && isThereOpponentPiece(auxp)) {
+			mat[auxp.getRow()][auxp.getColumn()] = true;
+		}
+		
+		//down:
+		
+		auxp.setValues(position.getRow() + 1, position.getColumn());
+		while (getBoard().positionExists(auxp) && !getBoard().thereIsAPiece(auxp)) {
+			mat[auxp.getRow()][auxp.getColumn()] = true;
+			auxp.setRow(auxp.getRow() + 1);
+		}
+		if (getBoard().positionExists(auxp) && isThereOpponentPiece(auxp)) {
+			mat[auxp.getRow()][auxp.getColumn()] = true;
+		}
+		
+		//right:
+		
+		auxp.setValues(position.getRow(), position.getColumn() + 1);
+		while (getBoard().positionExists(auxp) && !getBoard().thereIsAPiece(auxp)) {
+			mat[auxp.getRow()][auxp.getColumn()] = true;
+			auxp.setColumn(auxp.getColumn() + 1);
+		}
+		if (getBoard().positionExists(auxp) && isThereOpponentPiece(auxp)) {
+			mat[auxp.getRow()][auxp.getColumn()] = true;
+		}
+		
+		//left:
+		
+		auxp.setValues(position.getRow(), position.getColumn() - 1);
+		while (getBoard().positionExists(auxp) && !getBoard().thereIsAPiece(auxp)) {
+			mat[auxp.getRow()][auxp.getColumn()] = true;
+			auxp.setColumn(auxp.getColumn() - 1);
+		}
+		if (getBoard().positionExists(auxp) && isThereOpponentPiece(auxp)) {
+			mat[auxp.getRow()][auxp.getColumn()] = true;
+		}
+		
 		return mat;
 	}
 }
